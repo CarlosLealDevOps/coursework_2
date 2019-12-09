363aad82578924eaec120147cbf3639cc3c28f49
Ubuntu <username@VM.3wic4chu4hje1nrjd5ghu1krrf.bx.internal.cloudapp.net>
2019-12-09 14:16:15 +0000

    test2 print line

--- testHelloWorld.java
+++ testHelloWorld.java
@@ -8,6 +8,6 @@ class testHelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World");
-	System.out.println("Test, World"); 
+	System.out.println("Test2, World"); 
     } 
 }
